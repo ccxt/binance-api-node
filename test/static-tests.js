0c2960d5af52a54c320d76354b671e60219ffe22
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-10-22 12:22:16 +0100

    add more static prices

--- test/static-tests.js
+++ test/static-tests.js
@@ -2,13 +2,13 @@ import test from 'ava'
 import nock from 'nock';
 import Binance, { ErrorCodes } from 'index'
 
+// Warning: For now these tests can't run in parallel due to nock interceptors
 const binance = Binance()
 
 let interceptedUrl = null;
 let interceptedBody = null;
 
-test.beforeEach(t => {
-
+test.serial.beforeEach(t => {
     interceptedUrl = null;
     interceptedBody = null;
     nock(/.*/)
@@ -34,7 +34,31 @@ test.beforeEach(t => {
     });
 });
 
-test('[REST] Prices no symbol', async t => {
+test.serial('[REST] Prices no symbol', async t => {
     await binance.prices();
     t.is(interceptedUrl, 'https://api.binance.com/api/v3/ticker/price')
+})
+
+test.serial('[REST] Futures Prices no symbol', async t => {
+    await binance.futuresPrices();
+    t.is(interceptedUrl, 'https://fapi.binance.com/fapi/v1/ticker/price')
+})
+
+
+test.serial('[REST] Orderbook', async t => {
+    try {
+        await binance.book({ symbol: 'BTCUSDT' });
+    } catch (e) {
+        // it can throw an error because of the mocked response
+    }
+    t.is(interceptedUrl, 'https://api.binance.com/api/v3/depth?symbol=BTCUSDT')
+})
+
+test.serial('[REST] Futures Orderbook', async t => {
+    try {
+        await binance.futuresBook({ symbol: 'BTCUSDT' });
+    } catch (e) {
+        // it can throw an error because of the mocked response
+    }
+    t.is(interceptedUrl, 'https://fapi.binance.com/fapi/v1/depth?symbol=BTCUSDT')
 })
