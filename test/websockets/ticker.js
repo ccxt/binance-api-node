b74ac48a4a00a2fffaee6b405399d7f41c13ab8b
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-11-28 13:01:51 +0000

    update test

--- test/websockets/ticker.js
+++ test/websockets/ticker.js
@@ -135,7 +135,7 @@ test('[WS] allTickers', t => {
     return new Promise(resolve => {
         const clean = client.ws.allTickers(tickers => {
             t.truthy(Array.isArray(tickers))
-            t.is(tickers[0].eventType, '24hrTicker')
+            t.is(tickers[0].eventType, '24hrMiniTicker')
             checkFields(t, tickers[0], ['symbol', 'priceChange', 'priceChangePercent'])
             clean()
             resolve()
